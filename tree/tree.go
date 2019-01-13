41bd176a36d1eeec4d97c34bc61fb93008811600
Go Sagawa <drunkturtle@gmail.com>
2019-01-13 19:47:08 +0900

    add tree function

--- tree/tree.go
+++ tree/tree.go
@@ -3,30 +3,68 @@ package tree
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+)
+
+//RelationLine ツリー表示がな文字列
+type RelationLine string
+
+const (
+	// RelationLineT T形
+	RelationLineT RelationLine = "├── "
+
+	// RelationLineI I形
+	RelationLineI RelationLine = "│   "
+
+	// RelationLineL L形
+	RelationLineL RelationLine = "└── "
+
+	// RelationLineEmpty ない場合
+	RelationLineEmpty RelationLine = "    "
 )
 
 //OutputTree tree実行
 func OutputTree(dir string, out io.Writer) error {
 
-	if dir == "dir_not_exist" {
-		return fmt.Errorf("dir not exist")
-	}
-
-	result := dir
-	result += `
-├── dir1
-│   ├── dir11
-│   │   └── file3
-│   ├── dir12
-│   │   └── file4
-│   └── file2
-└── file1
-`
-
-	_, err := out.Write([]byte(result))
+	result := fmt.Sprintf("%v\n", dir)
+	err := searchDir(dir, "", &result)
 	if err != nil {
 		return err
 	}
 
+	_, err = out.Write([]byte(result))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func searchDir(dir string, base string, result *string) error {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("cannnot read %v : %v", dir, err)
+	}
+	lastIndex := len(files) - 1
+	for i, file := range files {
+
+		var relationLineParent, relationLineChild RelationLine
+		if i == lastIndex {
+			relationLineParent = RelationLineL
+			relationLineChild = RelationLineEmpty
+		} else {
+			relationLineParent = RelationLineT
+			relationLineChild = RelationLineI
+		}
+		*result += fmt.Sprintf("%v%v%v\n", base, relationLineParent, file.Name())
+		if file.IsDir() {
+			nextDir := fmt.Sprintf("%v/%v", dir, file.Name())
+			nextBase := fmt.Sprintf("%v%v", base, relationLineChild)
+			err = searchDir(nextDir, nextBase, result)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -2,16 +2,28 @@ package tree
 
 import (
 	"bytes"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
 type AssertFn func(name string, err error)
 
+type unwritable struct{}
+
+func (u *unwritable) Write(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("this io.Write is unwritable")
+}
+
+const testdataDirCase1 = "testdata/case1"
+const testdataDirCase2 = "testdata/case2"
+
 func TestIsValidInput(t *testing.T) {
 
 	noError := func(name string, err error) {
 		if err != nil {
-			t.Errorf("%v: expected no error", name)
+			t.Errorf("%v: expected no error. error:%v", name, err)
 		}
 	}
 	withError := func(name string, err error) {
@@ -20,37 +32,51 @@ func TestIsValidInput(t *testing.T) {
 		}
 	}
 
-	dir := "testdata"
-	output := dir
+	home, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Chdir(home + "/../")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(os.Getwd())
+
+	output := testdataDirCase1
 	output += `
 ├── dir1
 │   ├── dir11
+│   │   ├── file2
 │   │   └── file3
-│   ├── dir12
-│   │   └── file4
-│   └── file2
+│   └── dir12
+│       ├── file4
+│       └── file5
 └── file1
 `
 	cases := []struct {
-		name     string
-		dir      string
-		output   string
-		assertFn AssertFn
+		name      string
+		dir       string
+		output    string
+		outStream interface{}
+		assertFn  AssertFn
 	}{
-		{name: "all correct", dir: dir, output: output, assertFn: noError},
-		{name: "dir_not_exist", dir: "dir_not_exist", output: "", assertFn: withError},
+		{name: "all correct", dir: testdataDirCase1, output: output, outStream: new(bytes.Buffer), assertFn: noError},
+		{name: "dir_not_exist", dir: "dir_not_exist", output: "", outStream: new(bytes.Buffer), assertFn: withError},
+		{name: "output error", dir: testdataDirCase1, output: output, outStream: new(unwritable), assertFn: withError},
+		{name: "directory_permission_error", dir: testdataDirCase2, output: output, outStream: new(unwritable), assertFn: withError},
 	}
 
 	for _, v := range cases {
 		t.Run(v.name, func(t *testing.T) {
-			outStream := new(bytes.Buffer)
-			err := OutputTree(v.dir, outStream)
+			err := OutputTree(v.dir, v.outStream.(io.Writer))
 			v.assertFn(v.name, err)
 			if err != nil {
 				return
 			}
-			if outStream.String() != v.output {
-				t.Errorf("%v: expected %v", outStream.String(), v.output)
+			buffer := v.outStream.(*bytes.Buffer)
+			if buffer.String() != v.output {
+				t.Errorf("%v\nexpected \n%v", buffer.String(), v.output)
 			}
 		})
 	}
