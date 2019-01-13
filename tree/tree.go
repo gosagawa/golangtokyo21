dab3b4dbbbe2b003cd79215bbec58641bfdad838
Go Sagawa <drunkturtle@gmail.com>
2019-01-13 23:51:27 +0900

    add deps command

--- tree/tree.go
+++ tree/tree.go
@@ -23,11 +23,30 @@ const (
 	RelationLineEmpty RelationLine = "    "
 )
 
+//Tree treeコマンド
+type Tree struct {
+	Deps int //取得する深さ
+}
+
+//NewTree treeコマンド作成
+func NewTree(deps int) (*Tree, error) {
+
+	tree := Tree{
+		Deps: deps,
+	}
+	err := tree.validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &tree, nil
+}
+
 //OutputTree tree実行
-func OutputTree(dir string, out io.Writer) error {
+func (t *Tree) OutputTree(dir string, out io.Writer) error {
 
 	result := fmt.Sprintf("%v\n", dir)
-	err := searchDir(dir, "", &result)
+	err := t.searchDir(dir, "", &result, 0)
 	if err != nil {
 		return err
 	}
@@ -40,11 +59,17 @@ func OutputTree(dir string, out io.Writer) error {
 	return nil
 }
 
-func searchDir(dir string, base string, result *string) error {
+func (t *Tree) searchDir(dir string, base string, result *string, deps int) error {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return fmt.Errorf("cannot read %v : %v", dir, err)
 	}
+
+	if t.isOverDeps(deps) {
+		return nil
+	}
+	deps++
+
 	lastIndex := len(files) - 1
 	for i, file := range files {
 
@@ -60,11 +85,29 @@ func searchDir(dir string, base string, result *string) error {
 		if file.IsDir() {
 			nextDir := fmt.Sprintf("%v/%v", dir, file.Name())
 			nextBase := fmt.Sprintf("%v%v", base, relationLineChild)
-			err = searchDir(nextDir, nextBase, result)
+			err = t.searchDir(nextDir, nextBase, result, deps)
 			if err != nil {
 				return err
 			}
 		}
 	}
 	return nil
+}
+
+func (t *Tree) validate() error {
+	if t.Deps < 0 {
+		return fmt.Errorf("deps must be over 0")
+	}
+	return nil
+}
+
+func (t *Tree) isOverDeps(deps int) bool {
+
+	if t.Deps == 0 {
+		return false
+	}
+	if t.Deps > deps {
+		return false
+	}
+	return true
 }
